09b8dd85d4c01a42d7396414e83e824d16295b3e
metaraine <raineorshine@gmail.com>
2015-08-02 19:36:47 -0600

    Add working Block toObject and subtract methods.

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
+var moment = require('moment')
+
+function Event() {
+}
+
+function Rule() {
+}
+
+function Block(start, end) {
+  this.start = start
+  this.end = end
+}
+
+Block.prototype.subtract = function (block) {
+  return [
+    new Block(this.start, block.start),
+    new Block(block.end, this.end)
+  ].filter(function (block) {
+    return block.end > block.start
+  })
+}
+
+Block.prototype.toObject = function () {
+  return {
+    start: this.start,
+    end:   this.end
+  }
+}
+
+function BlockArray() {
+}
+
 function freebusy(start, end, events, rules) {
 
 }
+
+freebusy.Event = Event
+freebusy.Block = Block
+freebusy.BlockArray = BlockArray
+freebusy.Rule = Rule
 
 module.exports = freebusy
--- test/spec.js
+++ test/spec.js
@@ -1,10 +1,83 @@
+var chai     = require('chai')
+var should   = chai.should()
+var moment   = require('moment')
 var freebusy = require('../index.js')
+
+var day0 = new Date;
+var day1 = moment(day0).subtract(1, 'days').toDate();
+var day2 = moment(day0).subtract(2, 'days').toDate();
+var day3 = moment(day0).subtract(3, 'days').toDate();
+var day7 = moment(day0).subtract(7, 'days').toDate();
+
+function blockToObject(block) {
+  return block.toObject()
+}
+
+describe('Block', function() {
+
+  describe('toObject', function() {
+    it('should convert the block to a simple object', function() {
+      (new freebusy.Block(day1, day0)).toObject().should.eql({
+        start: day1,
+        end: day0
+      })
+    })
+  })
+
+  describe('subtract', function() {
+
+    it('should return an empty array for an exact subtraction', function() {
+      var lastDay = new freebusy.Block(day1, day0)
+      lastDay.subtract(lastDay).should.eql([])
+    })
+
+    it('should return an empty array if a larger block is subtracted', function() {
+      (new freebusy.Block(day2, day1))
+      .subtract(new freebusy.Block(day3, day0))
+      .should.eql([])
+    })
+
+    it('should return an array with a smaller block if subtracting a block that overlaps the start', function() {
+      var blocks = (new freebusy.Block(day2, day0))
+        .subtract(new freebusy.Block(day3, day1))
+      blocks.map(blockToObject).should.eql([
+        { start: day1, end: day0 }
+      ])
+      blocks.should.have.length(1) // needed in case blocks is empty and map never runs
+    })
+
+    it('should return an array with a smaller block if subtracting a block that overlaps the end', function() {
+      var blocks = (new freebusy.Block(day3, day1))
+        .subtract(new freebusy.Block(day2, day0))
+      blocks.map(blockToObject).should.eql([
+        { start: day3, end: day2 }
+      ])
+      blocks.should.have.length(1) // needed in case blocks is empty and map never runs
+    })
+
+    it('should return an array with a two blocks if subtracting a block in the middle', function() {
+      var blocks = (new freebusy.Block(day3, day0))
+        .subtract(new freebusy.Block(day2, day1))
+      blocks.map(blockToObject).should.eql([
+        { start: day3, end: day2 },
+        { start: day1, end: day0 }
+      ])
+      blocks.should.have.length(2) // needed in case blocks is empty and map never runs
+    })
+
+  })
+
+})
 
 describe('freebusy', function() {
 
-  it('should take a start date, end date, list of busy events', function() {
-    // freebusy().should.equal({
-
+  it('should return a free day', function() {
+    freebusy(day1, day0, []).should.eql([
+      {
+        start: day1,
+        end: day0
+      }
+    ])
   })
 
 })
