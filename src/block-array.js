2cd42a7f6df7af1d107e4c42edecfe4b9baa2ed4
metaraine <raineorshine@gmail.com>
2015-08-16 21:32:00 -0600

    Move startOfDay and endOfDay logic to BlockArray.

--- src/block-array.js
+++ src/block-array.js
@@ -1,5 +1,11 @@
 var moment = require('moment')
-var Block = require('./block.js')
+var range  = require('lodash.range')
+var Block  = require('./block.js')
+
+// returns an 7-day array of the given value, or the value itself if it is already an array (converted from [Sun...Sat] to [Mon...Sun])
+function defaultWeek(x) {
+  return x && !Array.isArray(x) ? range(x, x+7, 0) : x
+}
 
 function BlockArray(blocks) {
   this.blocks = blocks || []
@@ -19,17 +25,22 @@ BlockArray.prototype.getBlocks = function () {
   return Array.prototype.slice.call(this.blocks)
 }
 
-BlockArray.days = function (start, end) {
-  var blockArray = new BlockArray()
+BlockArray.days = function (startDate, endDate, startOfDay, endOfDay) {
 
-  var current = moment(start);
-  while(current < end) {
-    var next = current.clone().add(1, 'days')
-    blockArray.blocks.push(new Block(current.toDate(), next.toDate()))
-    current = next
+  startOfDay = defaultWeek(startOfDay || 0)
+  endOfDay   = defaultWeek(endOfDay || 24)
+
+  var blocks = []
+  var current = moment(startDate);
+  while(current < endDate) {
+    var day = (current.day()+6)%7 // rotate number to use Sun as beginning of week
+    var start = current.clone().add(startOfDay[day], 'hours')
+    var end   = current.clone().add(endOfDay[day], 'hours')
+    blocks.push(new Block(start.toDate(), end.toDate()))
+    current = current.clone().add(1, 'days')
   }
 
-  return blockArray
+  return new BlockArray(blocks)
 }
 
 module.exports = BlockArray
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 var moment     = require('moment')
 var identity   = require('lodash.identity')
-var range      = require('lodash.range')
 var Block      = require('./block.js')
 var Rule       = require('./rule.js')
 var BlockArray = require('./block-array.js')
-
-// returns an 7-day array of the given value, or the value itself if it is already an array (converted from [Sun...Sat] to [Mon...Sun])
-function defaultWeek(x) {
-  return x && !Array.isArray(x) ? range(x, x+7, 0) : x
-}
 
 /**
  * @param args.start
@@ -22,33 +16,18 @@ function freebusy(args) {
 
   // defaults
   args.events     = args.events || []
-  args.startOfDay = defaultWeek(args.startOfDay)
-  args.endOfDay   = defaultWeek(args.endOfDay)
 
   var rules = args.rules ? args.rules.map(Rule.fromObject) : [Rule.true]
   var busyEvents = args.events.filter(λ(event) -> rules.some(λ.match(event)))
 
-  var freeDays =
-
-    // create free days from start till end
-    BlockArray.days(args.start, args.end).getBlocks()
-
-    // reduce the free time by startOfDay and endOfDay each day
-    .map(args.startOfDay || args.endOfDay ? function (day) {
-      var startDate = day.getStart()
-      var day = (startDate.getDay()+6)%7 // rotate number to use Sun as beginning of week
-      var startOfDay = args.startOfDay && args.startOfDay[day] || 0
-      var endOfDay   = args.endOfDay && args.endOfDay[day] || 24
-      var truncatedStart = moment(startDate).add(startOfDay, 'hours').toDate()
-      var truncatedEnd   = moment(startDate).add(endOfDay, 'hours').toDate()
-      return new Block(truncatedStart, truncatedEnd)
-    } : identity)
+  // create free days from start till end
+  var freeDays = BlockArray.days(args.start, args.end, args.startOfDay, args.endOfDay)
 
   // reduce freeDays by busy events
   // returning this full expression directly breaks sweetjs!
   var finalFreeTime = busyEvents.reduce(function (remainingTime, nextEvent) {
     return remainingTime.subtract(new Block(nextEvent.start, nextEvent.end))
-  }, new BlockArray(freeDays))
+  }, freeDays)
 
   return finalFreeTime.toObject()
 }
